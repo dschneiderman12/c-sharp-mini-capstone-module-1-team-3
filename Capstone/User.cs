a970af093541dbba64cdb34f4c5464720a757926
[Taylor Dupont] <Taylorjodupont96@gmail.com>
2022-02-10 17:30:53 -0500

    Updates taylor driving 2

--- Capstone/User.cs
+++ Capstone/User.cs
@@ -6,7 +6,7 @@ namespace Capstone
 {
     public class User //contains user interactions with the vending machine
     {
-        
+
         public static void MainMenu() //main menu displayed to user
         {
             Transactions transaction = new Transactions();
@@ -30,75 +30,75 @@ namespace Capstone
                 menu.PrintMenu();
             }
             string userPurchaseChoice = "";
+
+            if (userMainMenuChoice == "2")
             {
-                if (userMainMenuChoice == "2")
+                while (userPurchaseChoice != "3")
                 {
-                    while (userPurchaseChoice != "3")
+
+                    Console.WriteLine("(1) Feed Money");
+                    Console.WriteLine("(2) Select Product");
+                    Console.WriteLine("(3) Finish Transaction");
+
+                    userPurchaseChoice = Console.ReadLine();
+
+                    if (userPurchaseChoice == "1")
                     {
+                        Console.WriteLine("Please enter whole dollar amount (no decimal): ");
 
+                        string userMoneyInput = Console.ReadLine();
+                        transaction.FeedMoney(userMoneyInput);
+                    }
+                    if (userPurchaseChoice == "2")
+                    {
+                        menu.PrintMenu();
+                        Console.WriteLine("Select the location of your desired snack");
+                        string userSelection = Console.ReadLine().ToUpper();
 
-
-
-
-                        Console.WriteLine("(1) Feed Money");
-                        Console.WriteLine("(2) Select Product");
-                        Console.WriteLine("(3) Finish Transaction");
-
-                        userPurchaseChoice = Console.ReadLine();
-
-                        if (userPurchaseChoice == "1")
+                        if (menu.CanChangeQuantity(userSelection))
                         {
-                            Console.WriteLine("Please enter whole dollar amount (no decimal): ");
-
-                            string userMoneyInput = Console.ReadLine();
-                            transaction.FeedMoney(userMoneyInput);
-                        }
-                        if (userPurchaseChoice == "2")
-                        {
-                            menu.PrintMenu();
-                            Console.WriteLine("Select the location of your desired snack");
-                            string userSelection = Console.ReadLine().ToUpper();
-
-                            if (menu.CanChangeQuantity(userSelection))
+                            if (transaction.PurchaseItem(menu.ItemDictionary[userSelection].Price))
                             {
-                                if (transaction.PurchaseItem(menu.ItemDictionary[userSelection].Price))
-                                {
-                                    menu.ItemDictionary[userSelection].Quantity--;
-                                    Console.WriteLine($"Dispensing {menu.ItemDictionary[userSelection].ProductName} for {menu.ItemDictionary[userSelection].Price.ToString("C")}");
-                                    Console.WriteLine($"Remaining Balance: {transaction.Balance.ToString("C")}");
-                                    
-                                    if (menu.ItemDictionary[userSelection].Type == "Chip")
-                                    {
-                                        Console.WriteLine("Crunch Crunch, Yum!");
-                                    }
-                                    else if(menu.ItemDictionary[userSelection].Type == "Candy")
-                                    {
-                                        Console.WriteLine("Munch Munch, Yum!");
+                                menu.ItemDictionary[userSelection].Quantity--;
+                                Console.WriteLine($"Dispensing {menu.ItemDictionary[userSelection].ProductName} for {menu.ItemDictionary[userSelection].Price.ToString("C")}");
+                                Console.WriteLine($"Remaining Balance: {transaction.Balance.ToString("C")}");
 
-                                    }
-                                    else if(menu.ItemDictionary[userSelection].Type == "Drink")
-                                    {
-                                        Console.WriteLine("Glug Glug, Yum!");
-                                    }
-                                    else if(menu.ItemDictionary[userSelection].Type == "Gum")
-                                    {
-                                        Console.WriteLine("Chew Chew, Yum!");
-                                    }
+                                if (menu.ItemDictionary[userSelection].Type == "Chip")
+                                {
+                                    Console.WriteLine("Crunch Crunch, Yum!");
+                                }
+                                else if (menu.ItemDictionary[userSelection].Type == "Candy")
+                                {
+                                    Console.WriteLine("Munch Munch, Yum!");
+
+                                }
+                                else if (menu.ItemDictionary[userSelection].Type == "Drink")
+                                {
+                                    Console.WriteLine("Glug Glug, Yum!");
+                                }
+                                else if (menu.ItemDictionary[userSelection].Type == "Gum")
+                                {
+                                    Console.WriteLine("Chew Chew, Yum!");
                                 }
                             }
                         }
                     }
-                    if (userPurchaseChoice == "3")
-                    {
-                        Console.WriteLine($"Your Remaining Balance is {transaction.Balance.ToString("C")}.");
-                        transaction.GiveChange();
-                    
-                    }
+                }
+                if (userPurchaseChoice == "3")
+                {
+                    Console.WriteLine($"Your Remaining Balance is {transaction.Balance.ToString("C")}.");
+                    transaction.GiveChange();
+                    MainMenu();
                 }
             }
-            
+
+            if (userMainMenuChoice == "3")
+            {
+                Console.WriteLine("Thanks for shopping with us! Have a great day!");
+                
+            }
         }
         
-       
+
     }
 }
